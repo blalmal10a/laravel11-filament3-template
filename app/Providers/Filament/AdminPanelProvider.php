9c96489dae3072487dd93bafbb288ad5d10cbb53
ys-grouse <malsawma7777@gmail.com>
2024-09-20 00:24:05 +0530

    feat[add]: user profile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->profile()
             ->login()
             ->colors([
                 'primary' => Color::Amber,
