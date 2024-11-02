f6f0da584912ccb3ab8f3ecd7342e4770058673f
ys-grouse <malsawma7777@gmail.com>
2024-11-02 13:09:12 +0530

    feat[update]: script, landing page

--- winit.bat
+++ winit.bat
@@ -91,6 +91,19 @@ for /f "tokens=*" %%a in ('echo %str%') do (
 ) > .env
 
 echo .env file created successfully.
+
+composer install
+echo "Installation completed"
+php artisan migrate
+echo "Migration completed"
+php artisan storage:link
+php artisan key:generate
+php artisan db:seed
+
+echo "Seeding completed"
+
+php artisan serve
+
 goto :eof
 :tolower
 for %%a in ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z") do (
