42c2d173c299a436a7e4dfef8a758f6c802678bc
B. Lalmalsawma <45043442+blalmal10a@users.noreply.github.com>
2025-03-02 21:51:30 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\UserController;
 use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', LandingPage::class);
+
 Route::get('user-morph-one-image', [UserController::class, 'morph_one'])->name('demo.user.index');
 Route::get('images/{id}', [ImageController::class, 'getMedia']);
+Route::get('/', function(){
+    return 'hello koyeb';
+});
